771c059b7eadec55d3c5d32eda2f9d81cd382551
Chris <git@gbe0.com>
2021-03-10 13:59:27 +0000

    update provider dataclass

--- twoip/datatypes/provider.py
+++ twoip/datatypes/provider.py
@@ -3,6 +3,10 @@
 from dataclasses import dataclass
 from typing import List
 
+# Requirements for generating output (CSV or table)
+from csv import writer as csvwriter, QUOTE_NONNUMERIC
+from ipaddress import IPv4Address
+from io import StringIO
 from tabulate import tabulate
 
 from .providerresult import ProviderResult
@@ -42,16 +46,8 @@ class Provider(object):
             192.0.2.1                                64496      Some Company         https://www.example.com
             192.0.2.2                                64496      Some Company         https://www.example.com
         """
-        ## Create list to store formatted results
-        output = []
-
-        ## Loop over each result
-        for result in self.results:
-            ## Append the string representation to the output list
-            output.append(result.__str__())
-
-        ## Return the formatted output
-        return '\n'.join(output)
+        ## Output results as a table by default
+        return self.to_table()
 
     def to_dict(self) -> dict:
         """Return all Geo lookup results as a dict with a key by IP address
@@ -107,11 +103,56 @@ class Provider(object):
         ## Return results
         return result_dict
 
-    def to_table(self) -> str:
-        """Format the results into a table
+    @staticmethod
+    def __generate_headers(fields: List[str]) -> list:
+        """Generate headers from a list of fields
+
+        Args:
+            fields (List[str]): The list of fields to output
+
+        Returns:
+            list: The list of header data
+
+        Raises:
+            KeyError: Requested field does not exist
+            ValueError: Requested field is not allowed to be output into table
         """
-        ## Table header
-        headers: List[str] = [ 'IP Address', 'ASN', 'Name', 'Website' ]
+        ## Create a test ProviderResult object
+        result = ProviderResult(ipaddress = IPv4Address('192.0.2.0'))
+
+        ## Create dict to store table headers
+        headers = []
+
+        ## Make sure each field exists in the ProviderResult object
+        ## If the field does exist, the header will then be retrieved
+        for field in fields:
+
+            ## Check for field
+            if not hasattr(result, field):
+                raise KeyError(f'The requested field "{field}" does not exist in the ProviderResult object')
+
+            ## Get field title; if no title set it means the field can't be output to a table
+            try:
+                title = result.get_meta(field = field, name = 'title')
+            except KeyError:
+                raise ValueError(f'The requested field "{field}" cannot be output to a table')
+            else:
+                headers.append(title)
+
+        ## Return the headers
+        return headers
+
+    def to_table(self, fields: List[str] = ['ip', 'autonomous_system', 'name', 'website']) -> str:
+        """Format multiple provider lookup results into a table
+
+        Args:
+            fields (List[str], optional): The list of fields to output. Defaults to ['ip', 'autonomous_system', 'name', 'website'].
+
+        Returns:
+            str: The formatted table
+        """
+        ## Retrieve headers for table
+        headers = self.__generate_headers(fields = fields)
 
         ## Create list to store data of each result
         data: List[List[str]] = []
@@ -119,12 +160,49 @@ class Provider(object):
         ## Loop over each result
         for result in self.results:
 
-            ## Append result to data list
-            data.append([
-                f'{result.ip}',
-                f'{result.autonomous_system}',
-                f'{result.name}',
-                f'{result.website}'])
+            ## Create list to store the table data for this result
+            result_data: List[str] = []
+
+            ## Get each requested field, format as string and add to result data
+            for field in fields:
+                result_data.append(f'{getattr(result, field)}')
+
+            ## Add the fields to the table data
+            data.append(result_data)
 
         ## Generate and return table
         return tabulate(tabular_data = data, headers = headers, tablefmt = 'pretty')
+
+    def to_csv(self, fields: List[str] = ['ip', 'autonomous_system', 'name', 'website'], delimiter: str = ',') -> str:
+        """Format multiple provider lookup results into a CSV
+
+        Args:
+            fields (List[str], optional): The list of fields to output. Defaults to ['ip', 'autonomous_system', 'name', 'website'].
+            delimiter (str, optional): The field delimiter. Defaults to ','.
+
+        Returns:
+            str: The CSV
+        """
+        ## Create header row
+        data: List[List[str]] = self.__generate_headers(fields = fields)
+
+        ## Loop over each result
+        for result in self.results:
+
+            ## Create list to store the table data for this result
+            result_data: List[str] = []
+
+            ## Get each requested field, format as string and add to result data
+            for field in fields:
+                result_data.append(f'{getattr(result, field)}')
+
+            ## Add the fields to the table data
+            data.append(result_data)
+
+        ## Generate the CSV
+        output = StringIO()
+        writer = csvwriter(output, quoting = QUOTE_NONNUMERIC, delimiter = delimiter)
+        writer.writerows(data)
+
+        ## Return the CSV
+        return output.getvalue()
