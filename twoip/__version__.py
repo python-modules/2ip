8163d9b45b57246436a82f5193ba1d3dfa835591
Chris <git@gbe0.com>
2021-03-12 15:18:28 +0000

    set version to 1.0.0

--- twoip/__version__.py
+++ twoip/__version__.py
@@ -1,7 +1,7 @@
 __title__ = '2ip'
 __description__ = '2ip.me API Client.'
 __url__ = 'https://github.com/python-modules/2ip'
-__version__ = '0.0.2'
+__version__ = '1.0.0'
 __author__ = 'gbe0'
 __author_email__ = 'python@gbe0.com'
 __license__ = 'MIT'
