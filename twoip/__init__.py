1713390e83cc2e38d0590fc45860ffb23e515eef
Chris <git@gbe0.com>
2021-03-12 09:59:16 +0000

    revert logging change

--- twoip/__init__.py
+++ twoip/__init__.py
@@ -6,12 +6,12 @@ Python API client for 2ip.me
 This API client will send requests for various information to 2ip.me and provide a response in either XML or JSON.
 """
 
-# Import logging
+# Import logging and set null handler
 import logging
-from .logger import Logger
 
-# Enable logging
-Logger()
+# Set default logging handler to avoid "No handler found" warnings.
+from logging import NullHandler
+logging.getLogger(__name__).addHandler(NullHandler())
 
 # Import version info
 from .__version__ import __version__
