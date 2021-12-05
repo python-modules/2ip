5b43feeadfef643c78b4fb4b304ec2b849346fed
Chris <git@gbe0.com>
2021-12-05 15:56:19 +0000

    add ip param and url param for click

---
+++ twoip/cli/ipparam.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""
+Click Parameter Type - IP Address
+
+This parameter type will ensure the value provided is an IP address and convert it into the relevant IPv4Address or IPv6Address object.
+"""
+
+from click import ParamType
+from ipaddress import ip_address, IPv4Address, IPv6Address
+from typing import Union
+
+class IPAddressParam(ParamType):
+    """Validate the parameter is an IPv4/IPv6 address and normalize into an IPv4Address or IPv6Address object.
+
+    Examples:
+        '192.0.2.0' will become IPv4Address('192.0.2.0')
+        '2001:0DB8:0004:3f:00::2' will become IPv6Address('2001:db8:4:3f::2')
+    """
+
+    name = 'IP Address'
+
+    def convert(self, value: str, param, context) -> Union[IPv4Address, IPv6Address]:
+        """The function which will perform validation or normalization
+
+        Arguments:
+            value (str): The IPv4/IPv6 address
+
+        Returns:
+            Union[IPv4Address, IPv6Address]: The IPv4Address or IPv6Address object
+        """
+        ## Attempt address creation
+        try:
+            address: Union[IPv4Address, IPv6Address] = ip_address(value)
+        except ValueError:
+            ## Not a valid IPv4/IPv6 address
+            self.fail(f'Could not validate "{value!r}" as a valid IPv4 or IPv6 IP address')
+        except Exception as e:
+            ## Other exception types should be handled
+            self.fail(
+                f'Exception validating "{value!r}" as a valid IPv4 or IPv6 IP address: {e}',
+                param,
+                context,
+            )
+        else:
+            return address
---
+++ twoip/cli/urlparam.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+"""
+Click Parameter Type - URL
+
+This parameter type will ensure the value provided is a valid URL.
+"""
+
+from click import ParamType
+from urllib import parse
+
+class URLParam(ParamType):
+    """Validate the parameter is a valid URL
+
+    Examples:
+        'https://google.com' will pass validation
+        'fsd4g,d' will fail validation
+    """
+    name = 'URL'
+
+    def convert(self, value: str, param, context) -> str:
+        """The function which will perform validation or normalization
+
+        Arguments:
+            value (str): The URL to validate
+
+        Returns:
+            str: The validated URL
+        """
+        ## Attempt URL validation
+        try:
+            url = parse.urlparse(value)
+        except Exception as e:
+            ## Pass the exception to click
+            self.fail(
+                f'Exception validating "{value!r}" as a valid URL: {e}',
+                param,
+                context,
+            )
+
+        ## Make sure the URL is formed correctly
+        if all([url.scheme, url.netloc]):
+            if url.path:
+                return f"{url.scheme}://{url.netloc}{url.path}"
+            else:
+                return f"{url.scheme}://{url.netloc}"
+        else:
+            self.fail(
+                f'URL "{value!r}" is not correctly formed',
+                param,
+                context,
+            )
