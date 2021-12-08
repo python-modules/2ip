f1beed6ba2101fb9ba04ecb9a90d1963ae9ea833
Chris <git@gbe0.com>
2021-12-08 13:14:36 +0000

    format version file

--- twoip/__version__.py
+++ twoip/__version__.py
@@ -1,8 +1,17 @@
-__title__ = "2ip"
-__description__ = "2ip.me API Client - Python 3.6+ - Perform Geographic and Provider lookups for IP addresses using the 2ip.me API."
-__url__ = "https://github.com/python-modules/2ip"
-__version__ = "1.0.0"
-__author__ = "gbe0"
-__author_email__ = "python@gbe0.com"
-__license__ = "MIT"
-__copyright__ = "Copyright 2021 gbe0"
+# -*- coding: utf-8 -*-
+
+"""
+TwoIP API Client - Setuptools Variables
+"""
+
+__title__: str = "2ip"
+__description__: str = (
+    "2ip.me API Client - Python 3.6+ - Perform Geographic and "
+    "Provider lookups for IP addresses using the 2ip.me API."
+)
+__url__: str = "https://github.com/python-modules/2ip"
+__version__: str = "1.0.0"
+__author__: str = "gbe0"
+__author_email__: str = "python@gbe0.com"
+__license__: str = "MIT"
+__copyright__: str = "Copyright 2021 gbe0"
