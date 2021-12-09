2deaf9485260c5b9404cbc5ae1e646cdfd8794f8
Chris <git@gbe0.com>
2021-12-09 07:22:37 +0000

    add client import to init

--- twoip/__init__.py
+++ twoip/__init__.py
@@ -5,3 +5,5 @@ Python API client for 2ip.me
 
 This API client will send requests for various information to 2ip.me and provide a response in the requested format (eg. JSON, CSV or an object).
 """
+
+from .client import Client
