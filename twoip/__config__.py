1a6469865fb6315355bedf0f5af3f09578c87346
Chris <git@gbe0.com>
2021-12-09 05:00:53 +0000

    add config

--- twoip/__config__.py
+++ twoip/__config__.py
@@ -6,3 +6,12 @@ TwoIP API Client - Common Configuration
 
 ## The API URL
 __api__: str = "https://api.2ip.ua"
+
+## The URI for Geo lookups
+__uri_geo__: str = "/geo.json"
+
+## The URI for Geo lookups
+__uri_provider__: str = "/provider.json"
+
+## The logger name
+__logger__: str = "twoip"
