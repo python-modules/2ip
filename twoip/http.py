bd1e970f3fea529aeee7a7779a146a41b5873891
Chris <git@gbe0.com>
2021-12-10 15:21:22 +0000

    http template

---
+++ twoip/http.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+TwoIP API Client - HTTP Client
+
+This file provides the main API client for TwoIP.
+"""
+
+import httpx
+
+def HTTP:
+
+    """
+    TwoIP HTTP Client
+    """
+
