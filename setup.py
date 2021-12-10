7e7e6725ac526cb0a18e4a93d51a33ce9b78d5bd
Chris <git@gbe0.com>
2021-12-10 13:00:02 +0000

    remove python 3.6 from setup.py

--- setup.py
+++ setup.py
@@ -65,7 +65,6 @@ setup(
         "Natural Language :: English",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
