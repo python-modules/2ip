40605d6c27e7a231532e6a04a0a70a52a3a38734
Chris <git@gbe0.com>
2023-03-17 18:13:20 +0000

    define API URL constant

---
+++ twoip/constants/api.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+2IP API Client
+
+API configuration constants
+"""
+
+# pylint: disable=too-few-public-methods
+
+from enum import Enum
+
+
+class APISettings(Enum):
+    """
+    2IP API Client Settings
+    """
+
+    API_URL = "https://api.2ip.me"
